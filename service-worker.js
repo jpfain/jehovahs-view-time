dff9f49933375ab9cbe15e2a54e1ce37cf939e91
jpfain <jpfain@gmail.com>
2025-11-03 20:17:35 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,20 @@
-// service worker placeholder
+const CACHE_NAME = 'jehovahs-view-cache-v1';
+const urlsToCache = ['./', './index.html', './manifest.json', './icons/icon-192.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
